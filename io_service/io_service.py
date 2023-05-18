21d94027e94482d6bc59ee0e1d50e61c1d60dae6
NethDR <mihai.udubasa@yahoo.com>
2023-05-18 21:37:29 +0300

    add some crud paths

--- io_service/io_service.py
+++ io_service/io_service.py
@@ -2,42 +2,101 @@ from pymongo import MongoClient
 from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 import os
+import json
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 CORS(app)
 
 DATABASE = os.environ.get('MONGO_BL_DATABASE')
 COLLECTION = os.environ.get('MONGO_SESSIONS_COLLECTION')
-client = MongoClient('mongodb://mongo:27017/',
+client = MongoClient('mongodb://mongo_bl:27017/',
     username=os.environ.get('MONGO_INITDB_ROOT_USERNAME'),
     password=os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
 )
 
-print("Authenthication server connected to database! :)")
 
 db = client[DATABASE]
 collection = db[COLLECTION]
 
+collection.estimated_document_count()
+
+print("Authenthication server connected to database! :)")
+
+
 @app.route("/save_session", methods=["POST"])
-def register():
+def create():
     data = request.json
     userId = data.get('userId')
     health = data.get('health')
     attackPwr = data.get('attackPwr')
     shield = data.get('shield')
     level = data.get('level')
-    enemiesPos = data.get('enemiesPos')
-   
+    gameMap = data.get('map')
+    money = data.get("money")
+    scenarioId = data.get('scenarioId')
+
     session = {
         "userId": userId,
         "health" : health,
         "attackPwr" : attackPwr,
         "shield" : shield,
         "level" : level,
-        "enemiesPos" : enemiesPos
+        "money" : money,
+        "map" : gameMap,
+        "scenarioId" : scenarioId
     }
     result = collection.insert_one(session)
+    return Response(status=200, response=json.dumps(result.inserted_id.__str__()))
+
+@app.route("/save_session/<session_id>", methods=["PUT"])
+def update(session_id):
+    data = request.json
+    userId = data.get('userId')
+    health = data.get('health')
+    attackPwr = data.get('attackPwr')
+    shield = data.get('shield')
+    level = data.get('level')
+    gameMap = data.get('map')
+    money = data.get("money")
+    scenarioId = data.get('scenarioId')
+
+    session = {
+        "userId": userId,
+        "health" : health,
+        "attackPwr" : attackPwr,
+        "shield" : shield,
+        "level" : level,
+        "money" : money,
+        "map" : gameMap,
+        "scenarioId" : scenarioId
+    }
+    result = collection.update_one({"_id" : ObjectId(session_id)}, { "$set" : session})
+    if result.matched_count == 0:
+        return Response(status=404)
     return Response(status=200)
+
+
+@app.route("/get_session/<session_id>", methods=["GET"])
+def get(session_id):
+    result = collection.find_one(ObjectId(session_id))
+
+    if result is None:
+        return Response(status=404)
+    else:
+        session = {
+            "userId": result["userId"],
+            "health" : result["health"],
+            "attackPwr" : result["attackPwr"],
+            "shield" : result["shield"],
+            "level" : result["level"],
+            "money" : result["money"],
+            "map" : result["map"],
+            "scenarioId" : result["scenarioId"],
+        }
+        return Response(status=200, response=json.dumps(session))
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
